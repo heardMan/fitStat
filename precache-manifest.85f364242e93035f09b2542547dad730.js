360c8cec51a78785f8832be63390a2c22b867452
heardman <markyheard@gmail.com>
2019-12-07 21:55:05 -0800

    Updates

---
+++ precache-manifest.85f364242e93035f09b2542547dad730.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61db89c28c881827abb0494fb1317875",
+    "url": "/fitStat/index.html"
+  },
+  {
+    "revision": "fb9b43113df815c0405d",
+    "url": "/fitStat/static/js/2.a3bd9fd5.chunk.js"
+  },
+  {
+    "revision": "4efc63e4ded5d00178bd",
+    "url": "/fitStat/static/js/main.fe3b7173.chunk.js"
+  },
+  {
+    "revision": "28ae970544fa198b172f",
+    "url": "/fitStat/static/js/runtime-main.418e0237.js"
+  }
+]);
